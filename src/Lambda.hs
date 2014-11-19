e4c9472ff12e6d54ffddae61572bdbfb511904f2
Rens <de_kwibus@live.nl>
2014-11-19 17:06:19 +0100

    initial commit

---
+++ src/Lambda.hs
@@ -0,0 +1,18 @@
+module Lambda (
+)	where
+type Name = String
+
+data LamTerm= Lambda Name LamTerm 
+            | Appl LamTerm LamTerm 
+            | Var Name
+
+instance Show LamTerm where
+    show (Var n) = n
+    show (Lambda n t) = "\\" ++ n ++ "." ++ show t
+    show (Appl t1@ Lambda {} t2) = parentheses t1 ++ show t2
+    show (Appl t1@ Var {}     t2) = show t1 ++ show t2
+    show (Appl t1@ Appl {}    t2) = show t1 ++ show t2
+
+parentheses :: Show a => a -> String
+parentheses s = "(" ++ show s ++ ")"
+
