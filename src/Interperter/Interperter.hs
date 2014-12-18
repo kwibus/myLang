b1b656737bcfce1414838359440ffc248739b68d
Rens <de_kwibus@live.nl>
2014-12-18 22:40:41 +0100

    add repl

--- src/Interperter/Interperter.hs
+++ src/Interperter/Interperter.hs
@@ -1,7 +1,27 @@
 module Main
 where
-
+import Lambda
 import Parser
+import System.Console.Haskeline
 
-main :: IO()
-main = print $ parseString  "\\a.a"
+main :: IO ()
+main = runInputT defaultSettings loop
+ where
+    loop :: InputT IO ()
+    loop = do
+        minput <- getInputLine "% "
+        case minput of
+            Nothing -> return ()
+            Just "quit" -> return ()
+            Just input -> readEvalPrint input >> loop
+
+
+readEvalPrint :: String -> InputT IO ()
+readEvalPrint input = case parseString input of
+    Right ast -> outputStrLn $ pShow $ bruijn2Lam $ fullEval $ lam2Bruijn ast
+    Left e -> outputStrLn $ show e
+
+fullEval :: BruijnTerm -> BruijnTerm
+fullEval t = case eval t of
+    Nothing -> t
+    Just r -> fullEval r
