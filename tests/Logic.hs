43d765b94e90f54bb27a4175e0ba63bd0154eb10
Rens <de_kwibus@live.nl>
2015-03-26 04:54:34 +0100

    better backtracinkg logic

--- tests/Logic.hs
+++ tests/Logic.hs
@@ -7,24 +7,14 @@ import Control.Monad.Logic
 -- import Debug.Trace
 
 oneOfLogic :: [LogicT Gen a] -> LogicT Gen a
-oneOfLogic list = mychoose (length list) list
-    where mychoose _ [] = mzero
-          mychoose n l
-            | n > 1000 = mzero
-            | otherwise = do
-              i <- lift $ choose (0, n - 1)
-              let (result , rest) = extract i l
-              result `mplus` mychoose (n - 1) rest
+oneOfLogic list = do
+    a <- lift $ shuffle list
+    msum a
 
 elementsLogic :: [a] -> LogicT Gen a
-elementsLogic list = mychoose (length list) list
-    where mychoose _ [] = mzero
-          mychoose n l
-            | n > 1000 = mzero
-            | otherwise = do
-              i <- lift $ choose (0, n - 1)
-              let (result , rest) = extract i l
-              return result `mplus` mychoose (n - 1) rest
+elementsLogic list = do
+     a <- lift $ shuffle list
+     msum $ map return a
 
 chooseLogic :: Enum a => (a, a) -> LogicT Gen a
 chooseLogic (a, b) = elementsLogic [a .. b]
