aefb5317056f18a02dd062e89d67453204695378
Rens Sikma <de_kwibus@live.nl>
2016-03-11 12:20:54 +0100

    refactor testSolver

--- tests/TestTypeCheker.hs
+++ tests/TestTypeCheker.hs
@@ -3,8 +3,9 @@ module TestTypeCheker (testTypeChecker) where
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
--- import Control.Monad.Except
-import Data.IntMap
+import Control.Monad
+import qualified Data.IntMap as IM
+import Data.Bifunctor
 
 import qualified ExampleBruijn as B
 import MakeTerm
@@ -13,10 +14,13 @@ import ArbitraryType ()
 import TestUtils
 import ArbitraryLambda
 
+import BruijnTerm
+import PrettyPrint
+import Type
+import Lambda
 import TypeCheck
 import Operator
 import BruijnEnvironment
-import FreeEnvironment
 import TypeError
 import ErrorCollector
 
@@ -37,11 +41,11 @@ testApply = testGroup "apply"
     --     in apply t sub @?= t
 
     [ testCase " apply a {a-:b}}== b" $
-        let sub = finsertAt (tVar 2) (Free 1) fEmtyEnv
+        let sub =IM.singleton 1 (tVar 2)
         in apply sub (tVar 1) @?= tVar 2
 
     , testCase " apply a {a-:b, b-:c}}== c" $
-        let sub = fFromList [(tVar 2, Free 1), (tVar 3, Free 2)]
+        let sub = IM.fromList [(1,tVar 2), (2,tVar 3)]
         in apply sub (tVar 1) @?= tVar 3
     ]
 testUnify :: TestTree
@@ -54,20 +58,19 @@ testUnify = testGroup "unify"
 
     , testProperty "symetric " $
         \ t1 t2 -> unifys t1 t2 == unifys t2 t1
-
     ]
 
 testUnifySubs :: TestTree
 testUnifySubs = testGroup " UnifySubs "
     [testCase "unifySubs error" $
-        let sub1 = fromList [(1, tDouble) ]
-            sub2 = fromList [(1, tDouble ~> tDouble)]
+        let sub1 = IM.fromList [(1, tDouble) ]
+            sub2 = IM.fromList [(1, tDouble ~> tDouble)]
         in hasSucces (unifySubs sub1 sub2) @?= False
 
     , testCase "unifySubs error sum" $
-        let sub1 = fromList [ (1, tDouble)
+        let sub1 = IM.fromList [ (1, tDouble)
                             , (2, tDouble)]
-            sub2 = fromList [ (1, tDouble ~> tDouble)
+            sub2 = IM.fromList [ (1, tDouble ~> tDouble)
                             , (2, tDouble ~> tDouble)]
         in (case unifySubs sub1 sub2 of
                 Error es -> length es == 2
@@ -75,33 +78,33 @@ testUnifySubs = testGroup " UnifySubs "
            ) @?= True
 
     , testCase "unifySubs [a/b] [b/a]" $
-        let sub1 = fromList [(1, tVar 2)]
-            sub2 = fromList [(2, tVar 1)]
+        let sub1 = IM.fromList [(1, tVar 2)]
+            sub2 = IM.fromList [(2, tVar 1)]
         in unifySubs sub1 sub2 @?= return sub1
 
     , testCase " unifysSubs [a/(b ->c)]  [b/a] failse" $
-        let sub1 = fromList [(1,tVar 2 ~> tVar 3)]
-            sub2 = fromList [(2,tVar 1)]
+        let sub1 = IM.fromList [(1,tVar 2 ~> tVar 3)]
+            sub2 = IM.fromList [(2,tVar 1)]
         in hasSucces (unifySubs sub1 sub2 ) @?= False
 
     , testCase " unifysSubs [a/(b ->c)]  [a/b] fails" $
-        let sub1 = fromList [(1,tVar 2 ~> tVar 3)]
-            sub2 = fromList [(1,tVar 2)]
+        let sub1 = IM.fromList [(1,tVar 2 ~> tVar 3)]
+            sub2 = IM.fromList [(1,tVar 2)]
         in hasSucces (unifySubs sub1 sub2 ) @?= False
 
     , testCase " unifysSubs [b/a] [a/(b ->c)] failse" $
-        let sub2 = fromList [(1,tVar 2 ~> tVar 3)]
-            sub1 = fromList [(2,tVar 1)]
+        let sub2 = IM.fromList [(1,tVar 2 ~> tVar 3)]
+            sub1 = IM.fromList [(2,tVar 1)]
         in hasSucces (unifySubs sub1 sub2 ) @?= False
 
     , testCase " unifysSubs [a/b] [a/(b ->c)] fails" $
-        let sub2 = fromList [(1,(tVar 2) ~>(tVar 3))]
-            sub1 = fromList [(1,tVar 2)]
+        let sub2 = IM.fromList [(1,(tVar 2) ~>(tVar 3))]
+            sub1 = IM.fromList [(1,tVar 2)]
         in hasSucces (unifySubs sub1 sub2 ) @?= False
 
     , testCase " unifysSubs [a/b->c] [b/(a ->c)] fails" $
-        let sub2 = fromList [(1,tVar 2 ~> tVar 3)]
-            sub1 = fromList [(2,tVar 1 ~> tVar 3)]
+        let sub2 = IM.fromList [(1,tVar 2 ~> tVar 3)]
+            sub1 = IM.fromList [(2,tVar 1 ~> tVar 3)]
         in hasSucces (unifySubs sub1 sub2 ) @?= False
     ]
 
@@ -112,121 +115,9 @@ testClose = testGroup "close"
 
 testSolver :: TestTree
 testSolver = testGroup "Solver"
-   [ testCase "check Double" $
-        solver (double 1.0) @?= return tDouble
-   , testCase "check (+1)" $
-        solver (appl (val plus) (double 1.0)) @?=
-        return (tDouble ~> tDouble)
-   , testCase "check id" $
-        solver B.id @?= return (tVar 0 ~> tVar 0)
-
-   , testCase "check id 1.0" $
-        solver (appl B.id (double 1.0)) @?= return tDouble
-    , testCase "check \\a.1+a" $
-        solver (lambda "a" (appl
-                    (appl (val plus ) (double 1.0))
-                    (bvar 0)))
-                @?=
-        return (tDouble ~> tDouble)
-   , testCase "check \\a.a+1" $
-        solver (lambda "a" (appl
-                    (appl (val plus ) (bvar 0))
-                    (double 1.0)))
-                @?=
-        return (tDouble ~> tDouble)
-
-   , testCase "check (\\a\\b.a) 1" $
-        solver (appl
-                    (lambda "a" (lambda "b" (bvar 1)))
-                    (double 1)
-               )
-        @?=
-        return (tVar 0 ~> tDouble)
-
-   , testCase "check \\a\\b.b a" $
-        solver (lambda "a" (lambda "b" (appl
-                    (bvar 0)
-                    (bvar 1)
-                )))
-        @?=
-        return ( tVar 0 ~> (tVar 0 ~> tVar 1) ~> tVar 1 )
-
-   , testCase "check \\a.a a" $
-        solver (lambda "a" (appl
-                    (bvar 0)
-                    (bvar 0)
-                ))
-        @?=
-        throw [ UnifyAp undefined undefined undefined [Infinit undefined undefined ]]
-
-   , testCase "check (\\a.a (a 1.0))" $
-        solver (lambda "a" (appl
-                    (bvar 0)
-                    (appl (bvar 0)
-                          (double 1.0)
-                    )
-                ))
-        @?=
-        return ((tDouble ~> tDouble) ~> tDouble)
-
-   , testCase "check (\\f.\\a. f a a)" $
-        solver (lambda "f" (lambda "a" (appl
-                    (appl (bvar 1)
-                          (bvar 0))
-                    (bvar 0)
-                 )))
-        @?=
-        return (( tVar 0 ~> (tVar 0 ~> tVar 1)) ~> (tVar 0 ~>tVar 1))
-
-    , testCase "check (\\a.a)(\\b.\\c.b 1.0)" $
-        solver (appl (lambda "a " (bvar 0))
-                     (lambda "b" (lambda "c" (
-                            appl (bvar 1)
-                            (double 1.0)
-               ) )) )
-        @?=
-        return ((tDouble ~>tVar 0) ~> (tVar 1 ~> tVar 0))
-
-    , testCase "fail (+)\\a.a" $
-        solver (appl (val plus) B.id ) @?=
-        throw [UnifyAp undefined undefined undefined [Unify undefined undefined ]]
-
-    , testCase "fail \\a.a a" $
-        solver (lambda "a" (appl (bvar 0 ) (bvar 0))) @?=
-        throw [UnifyAp undefined undefined undefined [Infinit undefined undefined ]]
-
-    , testCase "(\\y.(\\x y)((\\z y)y(\\w.w)) )\\a.a " $
-        solver (appl (lambda "x" (appl (lambda "y" (bvar 1))
-                                       (appl (lambda "z" (bvar 1))
-                                             (appl (bvar 0)
-                                                   (lambda "w" (bvar 0))))))
-               B.id)
-        @?=
-        return ( (tVar 0 ~> tVar 0) ~> (tVar 0 ~>tVar 0))
-
-  , testCase "let id = \\a .a in id id" $
-        solver (mkLet [("id",lambda "a" (bvar 0))] (appl (bvar 0) (bvar 0)))
-        @?= return (tVar 0 ~> tVar 0)
-    -- TODO fail let polymorfise lambca
-    , testCase "\\id. ((\\ a b .a) (id 1.0) (id *))(\\a.a) " $
-        solver (appl (lambda "id "( lambda "c" (appl (appl
-                            (lambda "a" (lambda "b"(bvar 1)))
-                            (appl (bvar 0) (double 1.0)))
-                        (appl (bvar 0) (val plus)))))
-                (lambda "a" (bvar 0)))
-
-        @?= (throw [UnifySubs undefined  [Unify undefined undefined ] ])
-
-    , testCase "let id = \\a .a in (\\ a b .a) (id 1.0) (id +) " $
-        solver (mkLet [("id",lambda "a" (bvar 0))] (appl (appl
-                    (lambda "a" (lambda "b"(bvar 1)))
-                    (appl (bvar 0) (double 1.0)))
-               (appl (bvar 0) (val plus))
-            ))
-        @?= return tDouble
-
+    [ testGroup "testCases" (map (uncurry testCaseSolver) cassesSolver)
     , testProperty "idempotence" $
-        forAllTypedBruijn $ \ e -> case runInfer $ solveWith e fEmtyEnv bEmtyEnv of
+        forAllTypedBruijn $ \ e -> case runInfer $ solveWith e IM.empty bEmtyEnv of
                 Error _ -> False
                 Result (t1, sub1) -> case runInfer $ solveWith e sub1 bEmtyEnv of
                     Error _ -> False
@@ -235,3 +126,103 @@ testSolver = testGroup "Solver"
     , testProperty "typeable" $
         forAllTypedBruijn $ \ e -> hasSucces $ solver e
     ]
+cassesSolver :: [(LamTerm () Bound, ErrorCollector [TypeError ()] Type)]
+cassesSolver =
+    [ (double 1.0, return tDouble)
+
+    , (appl (val plus) (double 1.0), return (tDouble ~> tDouble))
+
+    , (B.id, return (tVar 0 ~> tVar 0))
+
+    , (appl B.id (double 1.0), return tDouble)
+
+    , (lambda "a" (appl
+            (appl (val plus ) (double 1.0))
+                  (bvar 0))
+      , return (tDouble ~> tDouble))
+    , (lambda "a" (appl
+                    (appl (val plus ) (bvar 0))
+                    (double 1.0))
+      , return (tDouble ~> tDouble))
+
+    , ((appl (lambda "a" (lambda "b" (bvar 1)))
+            (double 1))
+       , return (tVar 0 ~> tDouble))
+
+    , (lambda "a" (lambda "b" (appl
+                    (bvar 0)
+                    (bvar 1)
+                ))
+        , return ( tVar 0 ~> (tVar 0 ~> tVar 1) ~> tVar 1 ))
+
+    , (lambda "a" (appl
+                    (bvar 0)
+                    (bvar 0)
+                )
+          , throw [ UnifyAp undefined undefined undefined [Infinit undefined undefined ]])
+
+    , (lambda "a" (appl
+                    (bvar 0)
+                    (appl (bvar 0)
+                          (double 1.0)
+                    )
+                )
+       , return ((tDouble ~> tDouble) ~> tDouble))
+
+    , (lambda "f" (lambda "a" (appl
+                    (appl (bvar 1)
+                          (bvar 0))
+                    (bvar 0)
+                 ))
+        ,return (( tVar 0 ~> (tVar 0 ~> tVar 1)) ~> (tVar 0 ~>tVar 1)))
+
+    , (appl (lambda "a " (bvar 0))
+                     (lambda "b" (lambda "c" (
+                            appl (bvar 1)
+                            (double 1.0)
+               ) ))
+        , return ((tDouble ~>tVar 0) ~> (tVar 1 ~> tVar 0)))
+
+    , (appl (val plus) B.id
+       , throw [UnifyAp undefined undefined undefined [Unify undefined undefined ]])
+
+    , (lambda "a" (appl (bvar 0 ) (bvar 0))
+        , throw [UnifyAp undefined undefined undefined [Infinit undefined undefined ]])
+
+    , (appl (lambda "x" (appl (lambda "y" (bvar 1))
+                              (appl (lambda "z" (bvar 1))
+                                    (appl (bvar 0)
+                                          (lambda "w" (bvar 0))))))
+            B.id
+           , return ( (tVar 0 ~> tVar 0) ~> (tVar 0 ~>tVar 0)))
+
+    , (mkLet [("id",lambda "a" (bvar 0))] (appl (bvar 0) (bvar 0))
+        ,return (tVar 0 ~> tVar 0))
+
+    , (appl (lambda "id "( lambda "c" (appl (appl
+                            (lambda "a" (lambda "b"(bvar 1)))
+                            (appl (bvar 0) (double 1.0)))
+                        (appl (bvar 0) (val plus)))))
+                (lambda "a" (bvar 0))
+
+         ,throw [UnifySubs undefined  [Unify undefined undefined ] ])
+
+    , (mkLet [("id",B.id)] (appl (appl
+                    (lambda "a" (lambda "b"(bvar 1)))
+                    (appl (bvar 0) (double 1.0)))
+               (appl (bvar 0) (val plus))
+            )
+        , return tDouble)
+    ]
+
+testCaseSolver :: LamTerm () Bound -> ErrorCollector [TypeError ()] Type -> TestTree
+testCaseSolver lam expected = testCase (text ++ "solver " ++ lambdaDiscription ) $
+        unless (actualType == expected) (assertFailure errorMsg)
+    where
+      text = case expected of
+                Error _ -> "fail:"
+                _ -> ""
+      lambdaDiscription = either (const "coult not prinrt lambda") PrettyPrint.pShow (bruijn2Lam lam)
+      actualType = solver lam
+      errorMsg = "expected: " ++ show( second Type.pShow expected) ++
+               "\nbut got: " ++ show (second Type.pShow actualType)
