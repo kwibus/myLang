b0e73bf5648f6ec3e3de32e245ca7b4da1e027da
Rens Sikma <de_kwibus@live.nl>
2016-03-31 20:26:37 +0200

    dirty

--- tests/BackList.hs
+++ tests/BackList.hs
@@ -5,7 +5,9 @@ import Control.Applicative
 
 data BackList a = Failures !Int
                 | List  [a] (Maybe (BackList a))
-                deriving Show
+                | StepBacks Int
+                | Save (BackList a -> BackList a)
+                -- deriving Show
 
 instance Functor BackList where
   fmap = liftM
@@ -16,11 +18,19 @@ instance Applicative BackList where
 
 instance Monad BackList where
     return a = List [a] Nothing
+    Save c >>= f = Save $ f . c
     Failures i >>= _ = Failures i
     -- List [] >>= _ = error "emty backlist"
-    List (l:ls) next >>= f = setTop (Just (List ls next>>= f)) (f l)
+    List l next >>= f = setTop ((>>= f)<$>next) $ tryM (map f l) -- setTop (Just (List ls next>>= f)) (f l)
     List [] (Just next) >>= f = next >>= f
     List [] Nothing >>= _ = Failures 0
+
+mkSavePoint :: BackList a
+mkSavePoint = Save save
+
+save :: BackList a -> BackList a
+save (List (l:ls) next) = List [l] (Just (List ls  next))
+save a = a
 
 --  TODO remove is not mplus
 instance MonadPlus BackList where
@@ -30,10 +40,12 @@ instance MonadPlus BackList where
     mplus (List l next) b = List (l ++ trys b) next
 
 setTop :: Maybe(BackList a) -> BackList a -> BackList a
-setTop (Just a)(Failures i) = mplus (Failures i) a
-setTop Nothing (Failures i) = Failures i
+setTop (Just a) (Save f)  = f a
+setTop Nothing (Save f)  = Save f
 setTop newtop (List t Nothing ) = List t newtop
 setTop newtop (List t (Just oldtop)) = List t $ Just $  setTop newtop oldtop
+setTop (Just a)(Failures i) = mplus (Failures i) a
+setTop Nothing (Failures i) = Failures i
 
 instance Alternative BackList where
    empty = mzero
@@ -44,8 +56,7 @@ try [] = Failures 1
 try as = List as Nothing
 
 tryM :: [BackList a] -> BackList a
-tryM [] = Failures 1
-tryM list = foldr1 mplus list
+tryM list = foldl1 mplus list
 
 trys  :: BackList a -> [a]
 trys (Failures _) = []
