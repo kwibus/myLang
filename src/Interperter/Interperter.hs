7d341aac43acc68f37a89a794196db29d21197d1
Rens Sikma <de_kwibus@live.nl>
2016-12-21 17:38:27 +0100

    fix Interperter, now print fullEval expression

--- src/Interperter/Interperter.hs
+++ src/Interperter/Interperter.hs
@@ -2,14 +2,14 @@ module Main where
 
 import System.Console.Haskeline
 import Data.Either.Unwrap
-import Text.PrettyPrint.ANSI.Leijen
-import Control.Monad.IO.Class
+import Text.PrettyPrint.ANSI.Leijen as ANSI
 
+import Info
 import Parser
 import TypeCheck
 import BruijnTerm
 import Eval
-import Type
+import qualified Type
 import TypeError
 import ErrorCollector
 
@@ -22,20 +22,22 @@ main = runInputT defaultSettings loop
         case input of
             Nothing -> return ()
             Just "quit" -> return ()
-            Just input -> readEvalPrint input >> loop
-
+            Just expression -> readEvalPrint expression >> loop
 
 readEvalPrint :: String -> InputT IO ()
 readEvalPrint input = outPutDoc $ merge $ do
     ast <- mapLeft (text . show) $ parseString input
     bruijn <- mapLeft (text . show) $ lam2Bruijn ast
     t <- toEither $ mapError (showErrors input) $ solver bruijn
-    return $ text $ Type.pShow t
+    let result = fullEval $ removeInfo bruijn
+    return $ text ( Type.pShow t) ANSI.<$> text ( BruijnTerm.pShow result)
 
 merge :: Either a a -> a
 merge (Right a) = a
 merge (Left a) = a
 
 
+-- TODO this will give no color on Windows
+-- https://hackage.haskell.org/package/ansi-wl-pprint-0.6.7.3/docs/Text-PrettyPrint-ANSI-Leijen.html
 outPutDoc :: Doc -> InputT IO ()
-outPutDoc doc = liftIO $ putDoc $ doc <> line
+outPutDoc = outputStrLn . show
